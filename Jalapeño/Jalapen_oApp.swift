2b96caf1cd930a49dc02046339d611cbf18d31f5
JoeyShapiro <joeyashapiro@gmail.com>
2023-02-04 15:56:29 -0600

    Initial Commit

---
+++ Jalapeño/Jalapen_oApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jalapen_oApp.swift
+//  Jalapeño
+//
+//  Created by Joey Shapiro on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Jalapen_oApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
