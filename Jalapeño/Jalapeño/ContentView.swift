25f9544fc8a7b6a67090023fa874924843c25863
JoeyShapiro <joeyashapiro@gmail.com>
2023-02-12 15:51:38 -0600

    changed main list to custom image struct

--- Jalapeño/Jalapeño/ContentView.swift
+++ Jalapeño/Jalapeño/ContentView.swift
@@ -8,16 +8,32 @@
 import SwiftUI
 import LibJalapen_o
 
+struct WallpaperImage: Hashable {
+    let id: UUID
+    let name: String
+    let image: NSImage
+    var azimuth: Float = 0.0
+    var altitude: Float = 0.0
+    
+    public init(filePath: String) {
+        id = UUID()
+        name = filePath
+        image = NSImage(contentsOfFile: filePath) ?? NSImage(systemSymbolName: "square.and.arrow.up.trianglebadge.exclamationmark", accessibilityDescription: nil)!
+    }
+}
+
 struct ContentView: View {
-    // TODO if i just need image and not fs, use NSImage list
-    @State var images: [String] = ["/Users/oniichan/Pictures/WallpaperImagesR2/anime-computer-dawn-old.jpg", "/Users/oniichan/Pictures/WallpaperImagesR2/anime-computer-dawn-old.jpg"]
+    // it doesnt seem we need the path ever again. thank god, saves validating
+    @State var images: [WallpaperImage] = [ WallpaperImage(filePath: "/Users/oniichan/Pictures/WallpaperImagesR2/anime-computer-dawn-old.jpg"), WallpaperImage(filePath: "/Users/oniichan/Pictures/WallpaperImagesR2/anime-computer-dawn-old.jpg"), WallpaperImage(filePath: "garbage")
+    ]
     
     @State var primary = ""
     @State var forLight = ""
     @State var forDark = ""
     @State var curAzimuth: Float = 0.0
     @State var curAltitude: Float = 0.0
-    @State var selected = 0
+    @State var selected = UUID()
+    @State var curImage = WallpaperImage(filePath: "")
     
     var body: some View {
         let test = LibJalapen_o.init()
@@ -25,8 +41,12 @@ struct ContentView: View {
             HStack {
                 ScrollView {
                     ForEach(images, id: \.self) { image in
-                        WallpaperDisplay(filePath: image, height: 200, highlight: selected == 0, action: {
-                            selected = 0
+                        WallpaperDisplay(wallpaperImage: image, height: 200, highlight: selected == image.id, action: {
+                            
+                            
+                            selected = image.id
+                            curAzimuth = image.azimuth // TODO i dont think these update. also use an object
+                            curAltitude = image.altitude
                         })
                     }
                     WallpaperSystem(systemName: "plus", height: 200, action: {
@@ -38,24 +58,24 @@ struct ContentView: View {
                 VStack {
                     Picker("Primary", selection: $forLight) {
                         ForEach(images, id: \.self) { image in
-                            Text(image)
+                            Text(image.name)
                         }
                     }
                     Picker("For Light", selection: $forLight) {
                         ForEach(images, id: \.self) { image in
-                            Text(image)
+                            Text(image.name)
                         }
                     }
                     Picker("For Dark", selection: $forDark) {
                         ForEach(images, id: \.self) { image in
-                            Text(image)
+                            Text(image.name)
                         }
                     }
                     Divider()
                         .padding()
                     HStack {
                         Text("Azimuth")
-                        TextField("Azimuth", value: $curAzimuth, format: .number)
+                        TextField("Azimuth", value: $curImage.azimuth, format: .number)
                             .textFieldStyle(.roundedBorder) // TODO if blank, dont print text
                         Text("Altitude")
                         TextField("Alititude", value: $curAltitude, format: .number)
@@ -84,7 +104,7 @@ struct ContentView: View {
                             }
                         })
                         let realImages: [String] = possibleImages.filter({ f in return f != ""})
-                        images.append(contentsOf: realImages)
+                        images.append(contentsOf: realImages.map({ f in return WallpaperImage(filePath: f) })) // TODO super inefficient
                     }
                 } label: {
                     Image(systemName: "plus")
@@ -103,21 +123,22 @@ struct ContentView: View {
 }
 
 struct WallpaperDisplay: View {
-    var filePath: String
+    var wallpaperImage: WallpaperImage
     var height: CGFloat
     var highlight: Bool
     var action: () -> Void
     
     var body: some View {
-        let image = NSImage(contentsOfFile: filePath)
-        Image(nsImage: image!)
+        let image = wallpaperImage.image
+        let color = highlight ? Color.accentColor.opacity(0.8) : Color.accentColor.opacity(0)
+        Image(nsImage: image)
             .resizable()
             .aspectRatio(contentMode: .fill)
             .frame(width: height * 1.333, height: height)
             .clipped()
-            .border(Color.accentColor.opacity(0.8), width: 5)
+            .border(color, width: 5)
             .cornerRadius(10)
-            .help(filePath)
+            .help(wallpaperImage.name)
             .onTapGesture {
                 action()
             }
